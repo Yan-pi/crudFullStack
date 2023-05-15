b679e984071f074f4693a641db1f34e20a175553
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2023-05-15 20:02:01 -0300

    fixed mysql nomenclature bugs

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    passwor: "040916",
+    password: "040916",
     database: "crud",
 });
--- api/routes/users.js
+++ api/routes/users.js
@@ -4,3 +4,4 @@ import {getUsers} from "../controllers/user.js"
 const router = express.Router();
 router.get("/", getUsers);
 export default router;
+
