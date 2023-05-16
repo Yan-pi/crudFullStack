49d078d9689a2c2fcc9ab61a649f97f32a50978b
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2023-05-15 22:30:03 -0300

    Create components and Form

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
+import Global from './styles/global'
+import styled from "styled-components"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import Form from './Components/Form'
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
 
-
   return (
     <>
-    
+      <Container>
+        <Form />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <Global />
     </>
   )
 }
---
+++ fontend/src/Components/Form.jsx
@@ -0,0 +1,66 @@
+import { useRef } from "react";
+import styled from "styled-components";
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0 0 10px #ccc;
+  border-radius: 5px;
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label``;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+const Button = styled.button`
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d4;
+  color: white;
+  height: 42px;
+`;
+
+const Form = ({ onEdit }) => {
+  const ref = useRef();
+
+  return (
+    <FormContainer ref={ref}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="name" />
+      </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email" type="email" />
+      </InputArea>
+      <InputArea>
+        <Label>Phone</Label>
+        <Input name="phone" />
+      </InputArea>
+      <InputArea>
+        <Label>Birth Date</Label>
+        <Input name="brith_date" type="date" />
+      </InputArea>
+
+      <Button type="submit">Submit</Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
