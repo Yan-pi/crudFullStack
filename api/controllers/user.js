986de2cb052f991a09a2fa2048bd23677b983817
Yan Fernandes <102800044+Yan-pi@users.noreply.github.com>
2023-05-16 20:12:42 -0300

    Create addUser method

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -9,3 +9,20 @@ export const getUsers = (_, res) =>{
         return res.status(200).json(data);
     });
 };
+
+export const addUser = (req, res) =>{
+    const q = "INSERT INTO users ('name', 'email', 'phone', 'brith-date') VALUES (?)";
+    
+    const values =[
+        req.body.name,
+        req.body.email,
+        req.body.phone,
+        req.body.brithDate,
+    ]
+
+    db.query(q, req.body, (err, data)=>{
+        if (err) return res.json(err);
+
+        return res.status(200).json('User added successfully');
+    });
+}
